d3ba5d3e6861056ee025b1d7cc5b18568a595e75
Aster <galaster@foxmail.com>
2021-06-20 05:54:19 +0000

    🔮 Add either monad

---
+++ src/main/gen/valkyrie/language/psi/ValkyrieNamepathStar.java
@@ -0,0 +1,13 @@
+// This is a generated file. Not intended for manual editing.
+package valkyrie.language.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface ValkyrieNamepathStar extends PsiElement {
+
+  @NotNull
+  List<ValkyrieIdentifier> getIdentifierList();
+
+}
