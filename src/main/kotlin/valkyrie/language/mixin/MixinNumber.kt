cbf92f4d22f3c7c9a553f1efe2d6e0e9ec002e9d
Aster <galaster@foxmail.com>
2021-09-25 23:17:08 +0000

    🔮 Parse number as big integer

--- src/main/kotlin/valkyrie/language/mixin/MixinNumber.kt
+++ src/main/kotlin/valkyrie/language/mixin/MixinNumber.kt
@@ -5,6 +5,8 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.elementType
 import valkyrie.language.ast.ValkyrieASTBase
 import valkyrie.language.psi.ValkyrieTypes
+import java.math.BigDecimal
+import java.math.BigInteger
 
 // PsiReference
 open class MixinNumber(node: ASTNode) : ValkyrieASTBase(node) {
@@ -12,16 +14,31 @@ open class MixinNumber(node: ASTNode) : ValkyrieASTBase(node) {
         get() {
             return this.firstChild
         }
+
+    val integer: BigInteger?
+        get() = when (number.elementType) {
+            ValkyrieTypes.INTEGER -> BigInteger(number.text)
+            else -> null
+        }
+
+    val decimal: BigDecimal?
+        get() = when (number.elementType) {
+            ValkyrieTypes.DECIMAL -> BigDecimal(number.text)
+            else -> null
+        }
+
+    val byte: String?
+        get() = when (number.elementType) {
+            ValkyrieTypes.BYTE -> number.text
+            else -> null
+        }
+
     val unit: PsiElement?
         get() {
             val last = this.lastChild;
             return when (last.elementType) {
-                ValkyrieTypes.NUMBER_SUFFIX -> {
-                    last
-                }
-                else -> {
-                    null
-                }
+                ValkyrieTypes.NUMBER_SUFFIX -> last
+                else -> null
             }
         }
 
