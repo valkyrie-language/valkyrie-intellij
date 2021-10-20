24a8ce3f67ee11ceebdc404a9c50bbe89b284521
Aster <galaster@foxmail.com>
2021-10-20 16:59:04 +0000

    🐣 Add ValkyrieLibraryType and ValkyrieLibraryKind

---
+++ src/main/kotlin/valkyrie/ide/project/library/ValkyrieLibraryKind.kt
@@ -0,0 +1,15 @@
+package valkyrie.ide.project.library
+
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.libraries.PersistentLibraryKind
+
+class ValkyrieLibraryKind: PersistentLibraryKind<ValkyrieLibraryProperty>(ValkyrieLibraryType.ID) {
+    override fun createDefaultProperties(): ValkyrieLibraryProperty {
+        return ValkyrieLibraryProperty()
+    }
+
+    override fun getAdditionalRootTypes(): Array<OrderRootType> {
+        return super.getAdditionalRootTypes()
+    }
+
+}
--- src/main/kotlin/valkyrie/ide/project/library/ValkyrieLibraryType.kt
+++ src/main/kotlin/valkyrie/ide/project/library/ValkyrieLibraryType.kt
@@ -1,30 +1,41 @@
 package valkyrie.ide.project.library
 
-import com.intellij.openapi.roots.libraries.LibraryKind
-import com.intellij.openapi.roots.libraries.LibraryPresentationProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.libraries.LibraryType
+import com.intellij.openapi.roots.libraries.NewLibraryConfiguration
+import com.intellij.openapi.roots.libraries.ui.LibraryEditorComponent
+import com.intellij.openapi.roots.libraries.ui.LibraryPropertiesEditor
 import com.intellij.openapi.vfs.VirtualFile
 import valkyrie.language.file.ValkyrieIconProvider
 import javax.swing.Icon
+import javax.swing.JComponent
 
-class ValkyrieLibraryType : LibraryPresentationProvider<ValkyrieLibraryProperty>(kind) {
+/// 无需实现 library.presentationProvider
+class ValkyrieLibraryType() : LibraryType<ValkyrieLibraryProperty>(ValkyrieLibraryKind()) {
     companion object {
         const val ID = "library.valkyrie"
-        val kind: LibraryKind = LibraryKind.create(ID)
     }
 
+    override fun getCreateActionName(): String? {
+        return "ValkyrieLibraryType.getCreateActionName"
+    }
     override fun getDescription(properties: ValkyrieLibraryProperty): String? {
-        return "ValkyrieLibraryType.getDescription"
+        return  "ValkyrieLibraryType.getDescription"
+    }
+    override fun createNewLibrary(parentComponent: JComponent, contextDirectory: VirtualFile?, project: Project): NewLibraryConfiguration? {
+        return null
+    }
+
+    override fun createPropertiesEditor(editorComponent: LibraryEditorComponent<ValkyrieLibraryProperty>): LibraryPropertiesEditor? {
+        return null
     }
 
     override fun getIcon(properties: ValkyrieLibraryProperty?): Icon? {
         return ValkyrieIconProvider.FILE
     }
 
-    override fun detect(classesRoots: MutableList<VirtualFile>): ValkyrieLibraryProperty? {
-        for (root in classesRoots) {
-            println("ValkyrieLibraryType.detect: $root")
-        }
-        return ValkyrieLibraryProperty()
+    override fun getExternalRootTypes(): Array<OrderRootType> {
+        return super.getExternalRootTypes()
     }
-
 }
