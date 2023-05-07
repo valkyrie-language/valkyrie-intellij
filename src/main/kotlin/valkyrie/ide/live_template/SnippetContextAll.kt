8a74da1a38217a8c9b5a1bbe30d53c9c1b73e906
Aster <aster@vers.site>
2023-05-07 12:37:18 +0000

    🐤 Add more snippet contexts

---
+++ src/main/kotlin/valkyrie/ide/live_template/SnippetContextAll.kt
@@ -0,0 +1,9 @@
+package valkyrie.ide.live_template
+
+import com.intellij.codeInsight.template.EverywhereContextType
+import com.intellij.codeInsight.template.LiveTemplateContext
+import com.intellij.codeInsight.template.TemplateActionContext
+import com.intellij.codeInsight.template.TemplateContextType
+import valkyrie.language.ValkyrieBundle
+import valkyrie.language.ValkyrieLanguage
+
