7fad3ba987706ffa3f97c314a441c83be95c15a4
Aster <aster@vers.site>
2023-06-03 02:23:30 +0000

    🚀 Improve the lazy structure view

--- src/main/kotlin/valkyrie/ide/view/ValkyrieStructureItem.kt
+++ src/main/kotlin/valkyrie/ide/view/ValkyrieStructureItem.kt
@@ -1,212 +1,187 @@
 package valkyrie.ide.view
 
-import com.intellij.ide.structureView.StructureViewTreeElement
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.structureView.SearchableTextProvider
 import com.intellij.ide.util.treeView.smartTree.SortableTreeElement
+import com.intellij.ide.util.treeView.smartTree.TreeElement
 import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.NavigatablePsiElement
-import valkyrie.language.file.ValkyrieFileNode
-
+import com.intellij.psi.PsiFile
 import valkyrie.psi.node.*
 
 
-class ValkyrieStructureItem : StructureViewTreeElement, SortableTreeElement {
-    val node: NavigatablePsiElement
-    val view: ItemPresentation;
-    private var children: MutableList<ValkyrieStructureItem> = mutableListOf();
+open class ValkyrieStructureItem<T : NavigatablePsiElement> : SearchableTextProvider, SortableTreeElement {
+    protected val psi: T
     var visibility: ValkyrieVisibility = ValkyrieVisibility.Public
 
-    constructor(self: ValkyrieFileNode) {
-        this.node = self
-        this.view = self.presentation
-        // TODO: use visitor pattern
-        for (child in self.children) {
+    constructor(psi: T) {
+        this.psi = psi
+    }
+
+
+    override fun getValue(): NavigatablePsiElement = psi
+
+    override fun getSearchableText(): String? {
+        return psi.name
+    }
+
+    override fun navigate(requestFocus: Boolean) = psi.navigate(requestFocus)
+
+    override fun canNavigate(): Boolean = psi.canNavigate()
+
+    override fun canNavigateToSource(): Boolean = psi.canNavigateToSource()
+
+    override fun getAlphaSortKey(): String = psi.name ?: ""
+    override fun getPresentation(): ItemPresentation {
+        return psi.presentation ?: PresentationData()
+    }
+
+    override fun getChildren(): Array<TreeElement> {
+        val visitor = ValkyrieStructure()
+        psi.accept(visitor)
+        return visitor.items.toTypedArray()
+    }
+}
+
+
+class ValkyrieStructure : ValkyrieVisitor() {
+    val items: MutableList<TreeElement> = mutableListOf()
+    override fun visitFile(file: PsiFile) {
+        for (child in file.children) {
             when (child) {
-                is ValkyrieDeclareNamespaceNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareClassNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareUnionNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareTraitNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareFlagsNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareEnumerateNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareUniteNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieNewObjectNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieNewValueNode -> children.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareNamespaceNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareClassNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareUnionNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareTraitNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareFlagsNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareEnumerateNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareUniteNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieNewObjectNode -> items.add(ValkyrieStructureItem(child))
+                is ValkyrieNewValueNode -> items.add(ValkyrieStructureItem(child))
             }
         }
     }
 
-    constructor(self: ValkyrieDeclareNamespaceNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
+    override fun visitDeclareNamespace(o: ValkyrieDeclareNamespace) {
+        addAnnotations(o.annotations)
     }
 
-
-    constructor(self: ValkyrieDeclareFlagsNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        for (child in self.flagItems) {
+    override fun visitDeclareFlags(o: ValkyrieDeclareFlags) {
+        addAnnotations(o.annotations)
+        for (child in o.enumerateBody?.enumerateItemList ?: listOf()) {
             when (child) {
-                is ValkyrieDeclareSemanticNode -> children.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareSemanticNode -> o.add(ValkyrieStructureItem(child))
             }
         }
     }
 
-    constructor(self: ValkyrieDeclareEnumerateNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        for (child in self.enumerateItems) {
+    override fun visitDeclareEnumerate(o: ValkyrieDeclareEnumerate) {
+        addAnnotations(o.annotations)
+        for (child in o.enumerateBody?.enumerateItemList ?: listOf()) {
             when (child) {
-                is ValkyrieDeclareSemanticNode -> children.add(ValkyrieStructureItem(child))
+                is ValkyrieDeclareSemanticNode -> o.add(ValkyrieStructureItem(child))
             }
         }
     }
 
 
-    constructor(self: ValkyrieDeclareClassNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        addClassBody(self.classBody)
-    }
+//
+//    constructor(self: ValkyrieDeclareClassNode) {
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//        addClassBody(self.classBody)
+//    }
+//
+//    constructor(self: ValkyrieDeclareUnionNode) {
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//        addClassBody(self.classBody)
+//    }
+//
+//    constructor(self: ValkyrieDeclareUniteNode) {
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//        for (child in self.uniteItems) {
+//            when (child) {
+//                is ValkyrieDeclareVariantNode -> children.add(ValkyrieStructureItem(child))
+//                is ValkyrieDeclareMethodNode -> children.add(ValkyrieStructureItem(child))
+//            }
+//        }
+//    }
+//
+//    constructor(self: ValkyrieDeclareTraitNode) {
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//        addClassBody(self.classBody)
+//    }
+//
+//    constructor(self: ValkyrieDeclareSemanticNode) {
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//    }
+//
+//    constructor(self: ValkyrieDeclareVariantNode) {
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//        addClassBody(self.classBody)
+//    }
+//
 
-    constructor(self: ValkyrieDeclareUnionNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        addClassBody(self.classBody)
-    }
-
-    constructor(self: ValkyrieDeclareUniteNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        for (child in self.uniteItems) {
-            when (child) {
-                is ValkyrieDeclareVariantNode -> children.add(ValkyrieStructureItem(child))
-                is ValkyrieDeclareMethodNode -> children.add(ValkyrieStructureItem(child))
-            }
-        }
-    }
-
-    constructor(self: ValkyrieDeclareTraitNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        addClassBody(self.classBody)
-    }
-
-    constructor(self: ValkyrieDeclareSemanticNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-    }
-
-    constructor(self: ValkyrieDeclareVariantNode) {
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-        addClassBody(self.classBody)
-    }
-
+    //
+//    constructor(self: ValkyrieAttributeItem) {
+//        self as ValkyrieAttributeItemNode
+//        this.psi = self
+//    }
+//
+//    constructor(self: ValkyrieModifier) {
+//        self as ValkyrieModifierNode
+//        this.psi = self
+//    }
+//
+//    private fun addClassBody(body: ValkyrieClassBody?) {
+//        body ?: return
+//        for (item in body.declareFieldList) {
+//            children.add(ValkyrieStructureItem(item))
+//        }
+//        for (item in body.declareMethodList) {
+//            children.add(ValkyrieStructureItem(item))
+//        }
+//        for (item in body.declareDomainList) {
+//            children.add(ValkyrieStructureItem(item))
+//        }
+//    }
+//
+//    constructor(self: ValkyrieDeclareField) {
+//        self as ValkyrieDeclareFieldNode
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//    }
+//
+//    constructor(self: ValkyrieDeclareMethod) {
+//        self as ValkyrieDeclareMethodNode
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//    }
+//
+//    constructor(self: ValkyrieDeclareDomain) {
+//        self as ValkyrieDeclareDomainNode
+//        this.psi = self
+//        addAnnotations(self.annotations)
+//    }
+//
+//    constructor(self: ValkyrieNewObjectNode) {
+//        this.psi = self
+//    }
+//
+//    constructor(self: ValkyrieNewValueNode) {
+//        this.psi = self
+//    }
     private fun addAnnotations(annotations: ValkyrieAnnotations) {
         for (attribute in annotations.attributeBelowList) {
-            children.add(ValkyrieStructureItem(attribute.attributeItem))
+            items.add(ValkyrieStructureItem(attribute.attributeItem))
         }
         for (child in annotations.modifierList) {
-            children.add(ValkyrieStructureItem(child))
+            items.add(ValkyrieStructureItem(child))
         }
     }
-
-    constructor(self: ValkyrieAttributeItem) {
-        self as ValkyrieAttributeItemNode
-        this.node = self
-        this.view = self.presentation
-    }
-
-    constructor(self: ValkyrieModifier) {
-        self as ValkyrieModifierNode
-        this.node = self
-        this.view = self.presentation
-    }
-
-    private fun addClassBody(body: ValkyrieClassBody?) {
-        body ?: return
-        for (item in body.declareFieldList) {
-            children.add(ValkyrieStructureItem(item))
-        }
-        for (item in body.declareMethodList) {
-            children.add(ValkyrieStructureItem(item))
-        }
-        for (item in body.declareDomainList) {
-            children.add(ValkyrieStructureItem(item))
-        }
-    }
-
-    constructor(self: ValkyrieDeclareField) {
-        self as ValkyrieDeclareFieldNode
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-    }
-
-    constructor(self: ValkyrieDeclareMethod) {
-        self as ValkyrieDeclareMethodNode
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-    }
-
-    constructor(self: ValkyrieDeclareDomain) {
-        self as ValkyrieDeclareDomainNode
-        this.node = self
-        this.view = self.presentation
-        addAnnotations(self.annotations)
-    }
-
-    constructor(self: ValkyrieNewObjectNode) {
-        this.node = self
-        this.view = self.presentation
-//        addAnnotations(self.annotations)
-    }
-
-    constructor(self: ValkyrieNewValueNode) {
-        this.node = self
-        this.view = self.presentation
-//        addAnnotations(self.annotations)
-    }
-
-    override fun getValue(): Any = node
-
-    override fun navigate(requestFocus: Boolean) = node.navigate(requestFocus)
-
-    override fun canNavigate(): Boolean = node.canNavigate()
-
-    override fun canNavigateToSource(): Boolean = node.canNavigateToSource()
-
-    override fun getAlphaSortKey(): String = node.name ?: ""
-
-    override fun getPresentation(): ItemPresentation = view
-
-    override fun getChildren(): Array<out ValkyrieStructureItem> {
-        return children.toTypedArray()
-    }
-
-
 }
-//
-//class ValkyrieFoldVisitor : ValkyrieVisitor() {
-//    val items: MutableList<ValkyrieStructureItem> = mutableListOf()
-//    override fun visitDeclareNamespace(o: ValkyrieDeclareNamespace) {
-//        items.add(ValkyrieStructureItem(o as ValkyrieDeclareNamespaceNode))
-//    }
-//
-//    override fun visitDeclareClass(o: ValkyrieDeclareClass) {
-//        items.add(ValkyrieStructureItem(o as ValkyrieDeclareClassNode))
-//    }
-//
-//    override fun visitDeclareField(o: ValkyrieDeclareField) {
-//        items.add(ValkyrieStructureItem(o as ValkyrieDeclareFieldNode))
-//    }
-//}
