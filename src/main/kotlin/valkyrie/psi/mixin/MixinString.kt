fb2b5699db6c79bf39264cf23a09dd61487ebc40
Aster <aster@vers.site>
2023-06-10 16:20:23 +0000

    🧪 Implement multi-line string capture

--- src/main/kotlin/valkyrie/psi/mixin/MixinString.kt
+++ src/main/kotlin/valkyrie/psi/mixin/MixinString.kt
@@ -1,20 +1,24 @@
 package valkyrie.psi.mixin
 
-import com.intellij.json.json5.Json5Language
 import com.intellij.lang.ASTNode
-import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.LiteralTextEscaper
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiLanguageInjectionHost
-import org.intellij.lang.regexp.RegExpLanguage
 import valkyrie.psi.ValkyrieElement
-import valkyrie.psi.node.ValkyrieIdentifierNode
+import valkyrie.psi.ValkyrieTypes
 import valkyrie.psi.node.ValkyrieString
 import valkyrie.psi.node.ValkyrieStringNode
 
 
 abstract class MixinString : ValkyrieElement, PsiLanguageInjectionHost, ValkyrieString {
-    constructor(node: ASTNode) : super(node)
+    var stringHead: PsiElement? = this.findChildByType(ValkyrieTypes.STRING_L)
+    var stringBody: PsiElement? = this.findChildByType(ValkyrieTypes.STRING_TEXT)
+    var stringTail: PsiElement? = this.findChildByType(ValkyrieTypes.STRING_R)
+
+    constructor(node: ASTNode) : super(node) {
+
+    }
 
     override fun isValidHost(): Boolean {
         return identifier != null
@@ -28,29 +32,10 @@ abstract class MixinString : ValkyrieElement, PsiLanguageInjectionHost, Valkyrie
         return YggdrasilRegexEscaper(this as ValkyrieStringNode)
     }
 
-    fun injectPerform(r: MultiHostRegistrar) {
-        val id = (identifier as? ValkyrieIdentifierNode)?.name;
-
-        when (id) {
-            null -> {
-
-            }
-
-            "json" -> {
-                val range = TextRange(1, textLength - 1);
-                r.startInjecting(Json5Language.INSTANCE).addPlace(null, null, this, range).doneInjecting()
-            }
-
-            "re" -> {
-                val range = TextRange(1, textLength - 1);
-                r.startInjecting(RegExpLanguage.INSTANCE).addPlace(null, null, this, range).doneInjecting()
-            }
-
-            "regex" -> {
-                val range = TextRange(1, textLength - 1);
-                r.startInjecting(RegExpLanguage.INSTANCE).addPlace("(?x)", null, this, range).doneInjecting()
-            }
-        }
+    override fun subtreeChanged() {
+        stringHead = this.findChildByType(ValkyrieTypes.STRING_L)
+        stringBody = this.findChildByType(ValkyrieTypes.STRING_TEXT)
+        stringTail = this.findChildByType(ValkyrieTypes.STRING_R)
     }
 }
 
