60946a328ec7b1d9b4693eb148b36bd91da2c84d
Aster <aster@vers.site>
2023-06-12 18:38:49 +0000

    🐥 Support dynamic language inject parsing

--- src/main/kotlin/valkyrie/ide/matcher/LanguageInjector.kt
+++ src/main/kotlin/valkyrie/ide/matcher/LanguageInjector.kt
@@ -2,10 +2,13 @@ package valkyrie.ide.matcher
 
 
 import com.intellij.json.json5.Json5Language
+import com.intellij.lang.LanguageUtil
 import com.intellij.lang.injection.MultiHostInjector
 import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.refactoring.suggested.endOffset
+import com.intellij.refactoring.suggested.startOffset
 import org.intellij.lang.regexp.RegExpLanguage
 import valkyrie.psi.mixin.MixinString
 import valkyrie.psi.node.ValkyrieIdentifierNode
@@ -39,18 +42,26 @@ private class LanguageInjectorVisitor(val registrar: MultiHostRegistrar) : Valky
             }
 
             "json" -> {
-                val range = TextRange(1, o.textLength - 1);
+                val range = TextRange(o.stringHead.endOffset - o.startOffset, o.stringTail.startOffset - o.startOffset);
                 registrar.startInjecting(Json5Language.INSTANCE).addPlace(null, null, o, range).doneInjecting()
             }
 
             "re" -> {
-                val range = TextRange(1, o.textLength - 1);
+                val range = TextRange(o.stringHead.endOffset - o.startOffset, o.stringTail.startOffset - o.startOffset);
                 registrar.startInjecting(RegExpLanguage.INSTANCE).addPlace(null, null, o, range).doneInjecting()
             }
 
             "regex" -> {
-                val range = TextRange(1, o.textLength - 1);
+                val range = TextRange(o.stringHead.endOffset - o.startOffset, o.stringTail.startOffset - o.startOffset);
                 registrar.startInjecting(RegExpLanguage.INSTANCE).addPlace("(?x)", null, o, range).doneInjecting()
+            }
+
+            else -> {
+                val language = LanguageUtil.findRegisteredLanguage(id)
+                if (language != null) {
+                    val range = TextRange(o.stringHead.endOffset - o.startOffset, o.stringTail.startOffset - o.startOffset);
+                    registrar.startInjecting(language).addPlace(null, null, o, range).doneInjecting()
+                }
             }
         }
     }
