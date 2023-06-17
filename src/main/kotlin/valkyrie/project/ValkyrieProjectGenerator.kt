36e48d1825eec9573b645ed61f10f4a2cf91e73b
Aster <aster@vers.site>
2023-06-17 23:28:01 +0000

    🐤 Register the `ValkyrieFacetType`

--- src/main/kotlin/valkyrie/project/ValkyrieProjectGenerator.kt
+++ src/main/kotlin/valkyrie/project/ValkyrieProjectGenerator.kt
@@ -1,11 +1,11 @@
 package valkyrie.project
 
 import com.intellij.ide.util.projectWizard.WizardContext
-import com.intellij.ide.wizard.AbstractNewProjectWizardStep
 import com.intellij.ide.wizard.NewProjectWizardStep
 import com.intellij.ide.wizard.language.LanguageGeneratorNewProjectWizard
 import com.intellij.openapi.observable.util.bindBooleanStorage
 import com.intellij.openapi.observable.util.bindStorage
+import com.intellij.openapi.project.Project
 import com.intellij.ui.dsl.builder.BottomGap
 import com.intellij.ui.dsl.builder.Panel
 import com.intellij.ui.dsl.builder.bindSelected
@@ -14,11 +14,9 @@ import valkyrie.language.file.ValkyrieIconProvider
 import javax.swing.Icon
 
 
-class languageGenerator : LanguageGeneratorNewProjectWizard {
-    override val icon: Icon
-        get() = ValkyrieIconProvider.Instance.File
-    override val name: String
-        get() = "Valkyrie Project"
+class ValkyrieProjectGenerator : LanguageGeneratorNewProjectWizard {
+    override val icon: Icon = ValkyrieIconProvider.Instance.File
+    override val name: String = "Valkyrie Project"
 
     override fun createStep(parent: NewProjectWizardStep): NewProjectWizardStep {
         return A(parent, "NewProjectWizard.ValkyrieState")
@@ -28,24 +26,31 @@ class languageGenerator : LanguageGeneratorNewProjectWizard {
         return super.isEnabled(context)
     }
 
+
 }
 
-private class A(parentStep: NewProjectWizardStep, store: String) : AbstractNewProjectWizardStep(parentStep) {
+private class A(parentStep: NewProjectWizardStep, store: String) : NewProjectWizardStep {
+    override val context = parentStep.context
+    override val data = parentStep.context
+    override val keywords = parentStep.keywords
+    override val propertyGraph = parentStep.propertyGraph
     private val mono = propertyGraph.property(true).bindBooleanStorage(store)
     private val path = propertyGraph.property("unknown compiler version").bindStorage(store)
 
     override fun setupUI(builder: Panel) {
         with(builder) {
+            row("row") {
+                textField().bindText(path)
+            }
             row("") {
                 checkBox("Monorepo")
                     .bindSelected(mono)
 //                    .whenStateChangedFromUi { logGitChanged() }
-            }
-                .bottomGap(BottomGap.SMALL)
-            row("row") {
-                textField().bindText(path)
-            }
-
+            }.bottomGap(BottomGap.SMALL)
         }
+    }
+
+    override fun setupProject(project: Project) {
+        super.setupProject(project)
     }
 }
