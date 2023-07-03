f8d58aa750e656cc8e83a25336d2abc6fb0714cb
Aster <aster@vers.site>
2023-07-03 09:25:24 +0000

    🐥 Support type check by is-expression

---
+++ src/main/gen/valkyrie/psi/node/ValkyrieIsStatement.java
@@ -0,0 +1,26 @@
+// This is a generated file. Not intended for manual editing.
+package valkyrie.psi.node;
+
+import java.util.List;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface ValkyrieIsStatement extends PsiElement {
+
+    @Nullable
+    ValkyriePatternLiteral getPatternLiteral();
+
+    @Nullable
+    ValkyriePatternObject getPatternObject();
+
+    @Nullable
+    ValkyriePatternSequence getPatternSequence();
+
+    @Nullable
+    ValkyriePatternUnapply getPatternUnapply();
+
+    @Nullable
+    ValkyrieTypeExpression getTypeExpression();
+
+}
