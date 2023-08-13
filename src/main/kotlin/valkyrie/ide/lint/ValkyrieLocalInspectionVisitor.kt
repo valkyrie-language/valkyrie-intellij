d2e97e3fe0a0d755346f15c79b4e6b216ac32994
Aster <aster@vers.site>
2023-08-13 17:14:35 +0000

    🐣 Add functional macro example

--- src/main/kotlin/valkyrie/ide/lint/ValkyrieLocalInspectionVisitor.kt
+++ src/main/kotlin/valkyrie/ide/lint/ValkyrieLocalInspectionVisitor.kt
@@ -3,21 +3,16 @@ package valkyrie.ide.lint
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.psi.HintedPsiElementVisitor
 import com.intellij.psi.PsiElement
 import valkyrie.psi.node.ValkyrieVisitor
 
-abstract class ValkyrieLocalInspectionVisitor : ValkyrieVisitor, HintedPsiElementVisitor {
+abstract class ValkyrieLocalInspectionVisitor : ValkyrieVisitor {
     private val _holder: ProblemsHolder
     protected val tool: LocalInspectionTool
 
     constructor(tool: LocalInspectionTool, holder: ProblemsHolder) : super() {
         this.tool = tool
         this._holder = holder
-    }
-
-    override fun getHintPsiElements(): MutableList<Class<*>> {
-        return mutableListOf()
     }
 
     protected fun PsiElement.registerProblem(description: String, vararg fixes: LocalQuickFix) {
