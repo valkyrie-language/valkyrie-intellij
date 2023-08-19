146fcad4c93cc78d39402fc3d14e9726ba29f027
Aster <aster@vers.site>
2023-08-19 11:09:20 +0000

    🐣 Add language server proxy

--- src/main/kotlin/valkyrie/lsp/ValkyrieLanguageServerDescriptor.kt
+++ src/main/kotlin/valkyrie/lsp/ValkyrieLanguageServerDescriptor.kt
@@ -3,16 +3,16 @@ package valkyrie.lsp
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.platform.lsp.api.Lsp4jClient
-import com.intellij.platform.lsp.api.LspServerNotificationsHandler
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
 import valkyrie.language.file.ValkyrieFileType
 
-class ValkyrieLanguageServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Valkyrie LSP") {
-    override val lspHoverSupport = true
-
+class ValkyrieLanguageServerDescriptor : ProjectWideLspServerDescriptor {
     override val lspServerListener = VspListener()
     override val lspCodeActionsSupport = VspCodeActionAdaptor()
+    override val lspDiagnosticsSupport = VspDiagnosticsAdaptor()
+    override val lspCompletionSupport = VspCompletionAdaptor()
+
+    constructor(project: Project) : super(project, "Valkyrie LSP")
 
     override fun isSupportedFile(file: VirtualFile): Boolean {
         return file.fileType is ValkyrieFileType
--- src/main/kotlin/valkyrie/lsp/VspCodeActionAdaptor.kt
+++ src/main/kotlin/valkyrie/lsp/VspCodeActionAdaptor.kt
@@ -1,7 +1,16 @@
 package valkyrie.lsp
 
+import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.customization.LspCodeActionsSupport
+import com.intellij.platform.lsp.api.customization.LspIntentionAction
+import org.eclipse.lsp4j.CodeAction
 
-class VspCodeActionAdaptor() : LspCodeActionsSupport() {
+class VspCodeActionAdaptor : LspCodeActionsSupport() {
+    override fun createIntentionAction(lspServer: LspServer, codeAction: CodeAction): LspIntentionAction? {
+        return super.createIntentionAction(lspServer, codeAction)
+    }
 
+    override fun createQuickFix(lspServer: LspServer, codeAction: CodeAction): LspIntentionAction? {
+        return super.createQuickFix(lspServer, codeAction)
+    }
 }
