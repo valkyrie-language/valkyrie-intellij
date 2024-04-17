6a03f4ecc88b97bdb5f2e77870979ddb58ffa3c3
Aster <aster@vers.site>
2024-04-17 19:20:01 +0800

    Define the module types

--- src/main/kotlin/valkyrie/project/modules/ValkyrieRootModule.kt
+++ src/main/kotlin/valkyrie/project/modules/ValkyrieRootModule.kt
@@ -3,19 +3,21 @@ package valkyrie.project.facet
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.PrimaryModuleManager
 import com.intellij.openapi.project.Project
+import valkyrie.project.modules.ValkyrieModule
 import java.nio.file.Paths
+import kotlin.io.path.exists
 
 class ValkyrieRootModule : PrimaryModuleManager() {
     override fun getPrimaryModule(project: Project): Module? {
         println("getPrimaryModule: $project{}")
         val root = project.basePath?.let { Paths.get(it) } ?: return null
         val ws = root.resolve("legions.json5");
-//        if (ws.exists()) {
-//            return ws
-//        } else {
-//            return null
-//        }
-        return null
+        if (ws.exists()) {
+            return ValkyrieModule()
+        } else {
+            return null
+        }
     }
-
 }
+
+
