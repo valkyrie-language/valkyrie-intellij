d2b96d8f42a772a2aeb9ba98d07bcd23b95e260f
Aster <aster@vers.site>
2024-04-19 20:30:45 +0800

    Support type check by is-expression

---
+++ src/main/gen/valkyrie/psi/node/ValkyrieIsStatementNode.java
@@ -0,0 +1,62 @@
+// This is a generated file. Not intended for manual editing.
+package valkyrie.psi.node;
+
+import java.util.List;
+
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+
+import static valkyrie.psi.ValkyrieTypes.*;
+
+import valkyrie.psi.ValkyrieElement;
+
+public class ValkyrieIsStatementNode extends ValkyrieElement implements ValkyrieIsStatement {
+
+    public ValkyrieIsStatementNode(@NotNull ASTNode node) {
+        super(node);
+    }
+
+    public void accept(@NotNull ValkyrieVisitor visitor) {
+        visitor.visitIsStatement(this);
+    }
+
+    @Override
+    public void accept(@NotNull PsiElementVisitor visitor) {
+        if (visitor instanceof ValkyrieVisitor) accept((ValkyrieVisitor) visitor);
+        else super.accept(visitor);
+    }
+
+    @Override
+    @Nullable
+    public ValkyriePatternLiteral getPatternLiteral() {
+        return findChildByClass(ValkyriePatternLiteral.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyriePatternObject getPatternObject() {
+        return findChildByClass(ValkyriePatternObject.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyriePatternSequence getPatternSequence() {
+        return findChildByClass(ValkyriePatternSequence.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyriePatternUnapply getPatternUnapply() {
+        return findChildByClass(ValkyriePatternUnapply.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyrieTypeExpression getTypeExpression() {
+        return findChildByClass(ValkyrieTypeExpression.class);
+    }
+
+}
---
+++ src/main/gen/valkyrie/psi/node/ValkyriePatternAtomicNode.java
@@ -0,0 +1,56 @@
+// This is a generated file. Not intended for manual editing.
+package valkyrie.psi.node;
+
+import java.util.List;
+
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+
+import static valkyrie.psi.ValkyrieTypes.*;
+
+import valkyrie.psi.ValkyrieElement;
+
+public class ValkyriePatternAtomicNode extends ValkyrieElement implements ValkyriePatternAtomic {
+
+    public ValkyriePatternAtomicNode(@NotNull ASTNode node) {
+        super(node);
+    }
+
+    public void accept(@NotNull ValkyrieVisitor visitor) {
+        visitor.visitPatternAtomic(this);
+    }
+
+    @Override
+    public void accept(@NotNull PsiElementVisitor visitor) {
+        if (visitor instanceof ValkyrieVisitor) accept((ValkyrieVisitor) visitor);
+        else super.accept(visitor);
+    }
+
+    @Override
+    @Nullable
+    public ValkyrieIdentifier getIdentifier() {
+        return findChildByClass(ValkyrieIdentifier.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyrieNumber getNumber() {
+        return findChildByClass(ValkyrieNumber.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyrieSpecial getSpecial() {
+        return findChildByClass(ValkyrieSpecial.class);
+    }
+
+    @Override
+    @Nullable
+    public ValkyrieString getString() {
+        return findChildByClass(ValkyrieString.class);
+    }
+
+}
