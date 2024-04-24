c3aea061ce18172c96d521715bb959634286128b
Aster <aster@vers.site>
2024-04-24 14:57:31 +0800

    Load the Valkyrie Legion project

--- src/main/kotlin/valkyrie/project/artifact/ValkyrieArtifactType.kt
+++ src/main/kotlin/valkyrie/project/artifact/ValkyrieArtifactType.kt
@@ -1,39 +1,39 @@
-//package valkyrie.project.artifact
-//
-//import com.intellij.packaging.artifacts.ArtifactTemplate
-//import com.intellij.packaging.artifacts.ArtifactType
-//import com.intellij.packaging.elements.CompositePackagingElement
-//import com.intellij.packaging.elements.PackagingElementOutputKind
-//import com.intellij.packaging.elements.PackagingElementResolvingContext
-//import com.intellij.packaging.impl.elements.ArtifactRootElementImpl
-//import com.intellij.packaging.ui.PackagingSourceItem
-//import valkyrie.language.file.ValkyrieIconProvider
-//import javax.swing.Icon
-//
-//class ValkyrieArtifactType : ArtifactType("artifactType", { "ArtifactType" }) {
-//    override fun getIcon(): Icon {
-//        return ValkyrieIconProvider.Instance.Valkyrie
-//    }
-//
-//    override fun getDefaultPathFor(kind: PackagingElementOutputKind): String? {
-//        return "target/debug/main.wasm"
-//    }
-//
-//    override fun getDefaultPathFor(sourceItem: PackagingSourceItem): String? {
-//        return "target/debug/main.wasm"
-//    }
-//
-//    override fun createRootElement(artifactName: String): CompositePackagingElement<*> {
-//        return ArtifactRootElementImpl()
-//    }
-//
-//    override fun getPresentableName(): String {
-//        return "GetPresentableName1231"
-//    }
-//
-//    override fun getNewArtifactTemplates(context: PackagingElementResolvingContext): MutableList<out ArtifactTemplate> {
-//        return super.getNewArtifactTemplates(context)
-//    }
-//
-//
-//}
+package valkyrie.project.artifact
+
+import com.intellij.packaging.artifacts.ArtifactTemplate
+import com.intellij.packaging.artifacts.ArtifactType
+import com.intellij.packaging.elements.CompositePackagingElement
+import com.intellij.packaging.elements.PackagingElementOutputKind
+import com.intellij.packaging.elements.PackagingElementResolvingContext
+import com.intellij.packaging.impl.elements.ArtifactRootElementImpl
+import com.intellij.packaging.ui.PackagingSourceItem
+import valkyrie.language.file.ValkyrieIconProvider
+import javax.swing.Icon
+
+class ValkyrieArtifactType : ArtifactType("artifactType", { "ArtifactType" }) {
+    override fun getIcon(): Icon {
+        return ValkyrieIconProvider.Instance.Valkyrie
+    }
+
+    override fun getDefaultPathFor(kind: PackagingElementOutputKind): String? {
+        return "target/debug/main.wasm"
+    }
+
+    override fun getDefaultPathFor(sourceItem: PackagingSourceItem): String? {
+        return "target/debug/main.wasm"
+    }
+
+    override fun createRootElement(artifactName: String): CompositePackagingElement<*> {
+        return ArtifactRootElementImpl()
+    }
+
+    override fun getPresentableName(): String {
+        return "GetPresentableName1231"
+    }
+
+    override fun getNewArtifactTemplates(context: PackagingElementResolvingContext): MutableList<out ArtifactTemplate> {
+        return super.getNewArtifactTemplates(context)
+    }
+
+
+}
--- src/main/kotlin/valkyrie/project/modules/ValkyrieModule.kt
+++ src/main/kotlin/valkyrie/project/modules/ValkyrieModule.kt
@@ -1,74 +1,46 @@
 package valkyrie.project.modules
 
 import com.intellij.diagnostic.ActivityCategory
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.client.ClientKind
 import com.intellij.openapi.extensions.ExtensionsArea
 import com.intellij.openapi.extensions.PluginDescriptor
 import com.intellij.openapi.extensions.PluginId
-import com.intellij.openapi.module.impl.ModuleEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Condition
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.workspace.jps.entities.ModuleId
+import com.intellij.platform.workspace.storage.MutableEntityStorage
+import com.intellij.platform.workspace.storage.VersionedEntityStorage
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.serviceContainer.PrecomputedExtensionModel
 import com.intellij.util.messages.MessageBus
-import valkyrie.ide.view.scope.ValkyrieProjectScope
+import com.intellij.workspaceModel.ide.legacyBridge.ModuleBridge
 import java.nio.file.Path
 
-class ValkyrieModule(val _project: Project) : ModuleEx {
-    override fun <T : Any?> getUserData(key: Key<T>): T? {
-        return null
-    }
+class ValkyrieModule(val _project: Project) : ModuleBridge {
+    override var diff: MutableEntityStorage?
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var entityStorage: VersionedEntityStorage
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override val moduleEntityId: ModuleId
+        get() = TODO("Not yet implemented")
 
-    override fun <T : Any?> putUserData(key: Key<T>, value: T?) {
-
-    }
-
-    override fun dispose() {
-
-    }
-
-
-    override fun getExtensionArea(): ExtensionsArea {
+    override fun callCreateComponents() {
         TODO("Not yet implemented")
     }
 
-    override fun <T : Any?> getComponent(interfaceClass: Class<T>): T {
+    override suspend fun callCreateComponentsNonBlocking() {
         TODO("Not yet implemented")
     }
 
-    override fun hasComponent(interfaceClass: Class<*>): Boolean {
-        return false
-    }
-
-    override fun isInjectionForExtensionSupported(): Boolean {
-        return false
-    }
-
-    override fun getMessageBus(): MessageBus {
-        TODO("Not yet implemented")
-    }
-
-    override fun isDisposed(): Boolean {
-        return false
-    }
-
-    override fun getDisposed(): Condition<*> {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> getService(serviceClass: Class<T>): T {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> instantiateClass(aClass: Class<T>, pluginId: PluginId): T {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> instantiateClass(className: String, pluginDescriptor: PluginDescriptor): T & Any {
-        TODO("Not yet implemented")
-    }
-
-    override fun <T : Any?> instantiateClassWithConstructorInjection(aClass: Class<T>, key: Any, pluginId: PluginId): T {
+    override fun clearScopesCache() {
         TODO("Not yet implemented")
     }
 
@@ -89,7 +61,7 @@ class ValkyrieModule(val _project: Project) : ModuleEx {
         TODO("Not yet implemented")
     }
 
-    override fun <T : Any?> loadClass(className: String, pluginDescriptor: PluginDescriptor): Class<T> {
+    override fun dispose() {
         TODO("Not yet implemented")
     }
 
@@ -97,67 +69,44 @@ class ValkyrieModule(val _project: Project) : ModuleEx {
         TODO("Not yet implemented")
     }
 
-    override fun getModuleFile(): VirtualFile? {
+    override fun hasComponent(interfaceClass: Class<*>): Boolean {
+        return false
+    }
+
+    override fun <T : Any?> getComponent(interfaceClass: Class<T>): T? {
         return null
     }
 
-    override fun getModuleNioFile(): Path {
+
+    override fun getDeprecatedModuleLevelMessageBus(): MessageBus {
         TODO("Not yet implemented")
     }
 
-    override fun getProject(): Project {
-        return _project
+    override fun getDisposed(): Condition<*> {
+        TODO("Not yet implemented")
     }
 
-    override fun getName(): String {
-        return "getName"
+    override fun getExtensionArea(): ExtensionsArea {
+        TODO("Not yet implemented")
     }
 
-    override fun isLoaded(): Boolean {
-        return true
-    }
-
-    override fun setOption(key: String, value: String?) {
-
-    }
-
-    override fun getOptionValue(key: String): String? {
-        return "getOptionValue 232"
-    }
-
-    override fun getModuleScope(): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
-    }
-
-    override fun getModuleScope(includeTests: Boolean): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
-    }
-
-    override fun getModuleWithLibrariesScope(): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
-    }
-
-    override fun getModuleWithDependenciesScope(): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
+    override fun getMessageBus(): MessageBus {
+        TODO("Not yet implemented")
     }
 
     override fun getModuleContentScope(): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
+        TODO("Not yet implemented")
     }
 
     override fun getModuleContentWithDependenciesScope(): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
+        TODO("Not yet implemented")
     }
 
-    override fun getModuleWithDependenciesAndLibrariesScope(includeTests: Boolean): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
+    override fun getModuleFile(): VirtualFile? {
+        TODO("Not yet implemented")
     }
 
-    override fun getModuleWithDependentsScope(): GlobalSearchScope {
-        return ValkyrieProjectScope(_project)
-    }
-
-    override fun getModuleTestsWithDependentsScope(): GlobalSearchScope {
+    override fun getModuleNioFile(): Path {
         TODO("Not yet implemented")
     }
 
@@ -165,12 +114,134 @@ class ValkyrieModule(val _project: Project) : ModuleEx {
         TODO("Not yet implemented")
     }
 
-    override fun clearScopesCache() {
+    override fun getModuleScope(): GlobalSearchScope {
         TODO("Not yet implemented")
     }
 
-    override fun getDeprecatedModuleLevelMessageBus(): MessageBus {
+    override fun getModuleScope(includeTests: Boolean): GlobalSearchScope {
         TODO("Not yet implemented")
     }
 
+    override fun getModuleTestsWithDependentsScope(): GlobalSearchScope {
+        TODO("Not yet implemented")
+    }
+
+    override fun getModuleWithDependenciesAndLibrariesScope(includeTests: Boolean): GlobalSearchScope {
+        TODO("Not yet implemented")
+    }
+
+    override fun getModuleWithDependenciesScope(): GlobalSearchScope {
+        TODO("Not yet implemented")
+    }
+
+    override fun getModuleWithDependentsScope(): GlobalSearchScope {
+        TODO("Not yet implemented")
+    }
+
+    override fun getModuleWithLibrariesScope(): GlobalSearchScope {
+        TODO("Not yet implemented")
+    }
+
+    override fun getName(): String {
+        return "ValkyrieModule"
+    }
+
+    override fun getOptionValue(key: String): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getProject(): Project {
+        return _project
+    }
+
+    override fun <T : Any?> getService(serviceClass: Class<T>): T {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any?> getUserData(key: Key<T>): T? {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun initFacets() {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any?> instantiateClass(aClass: Class<T>, pluginId: PluginId): T {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any?> instantiateClass(className: String, pluginDescriptor: PluginDescriptor): T & Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any?> instantiateClassWithConstructorInjection(aClass: Class<T>, key: Any, pluginId: PluginId): T {
+        TODO("Not yet implemented")
+    }
+
+    override fun isDisposed(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isInjectionForExtensionSupported(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isLoaded(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any?> loadClass(className: String, pluginDescriptor: PluginDescriptor): Class<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onImlFileMoved(newModuleFileUrl: VirtualFileUrl) {
+        TODO("Not yet implemented")
+    }
+
+    override fun <T : Any?> putUserData(key: Key<T>, value: T?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerComponents(
+        corePlugin: IdeaPluginDescriptor?,
+        modules: List<IdeaPluginDescriptorImpl>,
+        precomputedExtensionModel: PrecomputedExtensionModel?,
+        app: Application?,
+        listenerCallbacks: MutableList<in Runnable>?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun rename(newName: String, newModuleFileUrl: VirtualFileUrl?, notifyStorage: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setOption(key: String, value: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getModuleTypeName(): String? {
+        return super.getModuleTypeName()
+    }
+
+    override fun getModuleProductionSourceScope(): GlobalSearchScope {
+        return super.getModuleProductionSourceScope()
+    }
+
+    override fun getModuleTestSourceScope(): GlobalSearchScope {
+        return super.getModuleTestSourceScope()
+    }
+
+    override fun getModuleFilePath(): String {
+        return super.getModuleFilePath()
+    }
+
+    override fun <T : Any?> getServices(serviceClass: Class<T>, client: ClientKind?): MutableList<T> {
+        return super.getServices(serviceClass, client)
+    }
+
+    override fun <T : Any?> getServiceIfCreated(serviceClass: Class<T>): T? {
+        return super.getServiceIfCreated(serviceClass)
+    }
 }
