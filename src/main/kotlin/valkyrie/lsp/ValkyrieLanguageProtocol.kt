c749553de5cac6bff971a14a6ecd012f9f5044b0
Aster <aster@vers.site>
2024-05-11 15:21:10 +0800

    Add Language Server Protocol suport

--- src/main/kotlin/valkyrie/lsp/ValkyrieLanguageProtocol.kt
+++ src/main/kotlin/valkyrie/lsp/ValkyrieLanguageProtocol.kt
@@ -3,23 +3,22 @@ package valkyrie.lsp
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lang.lsWidget.LanguageServiceWidgetItem
+import com.intellij.platform.lsp.api.LspServer
+import com.intellij.platform.lsp.api.LspServerSupportProvider
+import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
+import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
 
-//import com.intellij.platform.lsp.api.LspServer
-//import com.intellij.platform.lsp.api.LspServerSupportProvider
-//import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
-//import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
-//
-//
-//class ValkyrieLanguageProtocol : LspServerSupportProvider {
-//    override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerStarter) {
-//
-//    }
-//
-//    override fun createLspServerWidgetItem(lspServer: LspServer, currentFile: VirtualFile?): LspServerWidgetItem? {
-//        return super.createLspServerWidgetItem(lspServer, currentFile)
-//    }
-//
-//    override fun createLspWidgetItems(project: Project, currentFile: VirtualFile?): List<LanguageServiceWidgetItem> {
-//        return super.createLspWidgetItems(project, currentFile)
-//    }
-//}
+
+class ValkyrieLanguageProtocol : LspServerSupportProvider {
+    override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerStarter) {
+
+    }
+
+    override fun createLspServerWidgetItem(lspServer: LspServer, currentFile: VirtualFile?): LspServerWidgetItem? {
+        return super.createLspServerWidgetItem(lspServer, currentFile)
+    }
+
+    override fun createLspWidgetItems(project: Project, currentFile: VirtualFile?): List<LanguageServiceWidgetItem> {
+        return super.createLspWidgetItems(project, currentFile)
+    }
+}
