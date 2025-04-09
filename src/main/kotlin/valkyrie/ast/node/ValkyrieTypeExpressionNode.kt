db8014a6fb91a004925786f3e31f03b22d12f3b1
Aster <galaster@foxmail.com>
2025-04-09 16:56:19 +0800

    🔧 Fix power operator

--- src/main/kotlin/valkyrie/ast/node/ValkyrieTypeExpressionNode.kt
+++ src/main/kotlin/valkyrie/ast/node/ValkyrieTypeExpressionNode.kt
@@ -3,9 +3,7 @@ package valkyrie.ast.node
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import com.intellij.lang.PsiBuilder
-import valkyrie.ast.EXPRESSION
 import valkyrie.ast.ParserMonad
-import valkyrie.psi.node.ValkyrieAtomicNode
 
 class ValkyrieTypeExpressionNode(node: ASTNode) : ASTWrapperPsiElement(node) {
     companion object : ParserMonad {
