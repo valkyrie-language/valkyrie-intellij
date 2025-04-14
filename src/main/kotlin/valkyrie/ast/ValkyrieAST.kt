1d26d20bf83e89ef73d5140b8a53f5d949e1daea
Aster <galaster@foxmail.com>
2025-04-14 20:20:48 +0800

    Support template else

--- src/main/kotlin/valkyrie/ast/ValkyrieAST.kt
+++ src/main/kotlin/valkyrie/ast/ValkyrieAST.kt
@@ -93,7 +93,9 @@ val LetStatement = ValkyrieAST("LET")
 val TermTuplePattern = ValkyrieAST("TermTuplePattern")
 val CasePattern = ValkyrieAST("CAST_PATTERN")
 
+val TemplateElse = ValkyrieAST("TEMPLATE_ELSE")
 
+val TemplateEnd = ValkyrieAST("TEMPLATE_END")
 
 val AnnotationArea = ValkyrieAST("ANNOTATION_AREA")
 val Annotation = ValkyrieAST("ANNOTATION")
