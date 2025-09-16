c1e93c97d47d1bbf1f5426639794262d1ec270cc
aster <192608617@qq.com>
2025-09-16 16:17:32 +0800

    Add anti-shake mechanism and optimize startup time

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,11 +38,12 @@ dependencies {
 //        }
 
         bundledPlugin("com.intellij.java")
+        bundledPlugin("com.intellij.modules.ultimate")
         bundledPlugin("com.intellij.modules.json")
         bundledPlugin("org.toml.lang")
         bundledPlugin("org.intellij.plugins.markdown")
 //        // https://plugins.jetbrains.com/plugin/227-psiviewer/versions
-//        plugin("PsiViewer", "252.23892.248")
+        plugin("PsiViewer", "252.23892.248")
 //        plugin("com.github.voml.neo_theme", "0.4.3")
 
         testFramework(TestFrameworkType.Platform)
