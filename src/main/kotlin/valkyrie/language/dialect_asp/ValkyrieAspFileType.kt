b61aab3279e36021cbc7933997bf292575af6e95
aster <192608617@qq.com>
2025-09-19 11:56:23 +0800

    sfc

--- src/main/kotlin/valkyrie/language/dialect_asp/ValkyrieAspFileType.kt
+++ src/main/kotlin/valkyrie/language/dialect_asp/ValkyrieAspFileType.kt
@@ -1,4 +1,4 @@
-package valkyrie.language.dialect_template
+package valkyrie.language.dialect_asp
 
 import com.intellij.openapi.fileTypes.LanguageFileType
 import org.jetbrains.annotations.Nls
@@ -8,10 +8,10 @@ import javax.swing.Icon
 /**
  * Valkyrie 模板文件类型定义
  */
-class ValkyrieTemplateFileType private constructor() : LanguageFileType(ValkyrieTemplateLanguage) {
+class ValkyrieAspFileType private constructor() : LanguageFileType(ValkyrieAspLanguage) {
     companion object {
         @JvmStatic
-        val INSTANCE = ValkyrieTemplateFileType()
+        val INSTANCE = ValkyrieAspFileType()
     }
 
     override fun getName(): String = "valkyrie-template"
